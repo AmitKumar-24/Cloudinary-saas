ce65fb000abdd84a5c6df603301473d34d8fc237
Amit kumar <codingboyamit@gmail.com>
2025-05-14 01:58:01 +0530

    changes in tailwind.config.ts file

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,16 +1,14 @@
-// const config = {
-//   plugins: ["@tailwindcss/postcss"],
-// };
-
-// export default config;
-
-
-// postcss.config.js
 const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
+
+
+
+// module.exports = {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//     autoprefixer: {},
+//   },
+// };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,8 +49,6 @@
 
 // export default config;
 
-
-// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -58,17 +56,17 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      }
+    }
   },
-  plugins: [require("daisyui")], // ✅ this is still okay
+  plugins: [require("daisyui")]
 };
 
 export default config;
